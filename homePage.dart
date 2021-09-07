dc1b5e5794dabd9385d8545a1c49b49e3139339f
Nainji <88496144+Nainji@users.noreply.github.com>
2021-09-07 23:40:48 +0530

    Add files via upload

---
+++ homePage.dart
@@ -0,0 +1,306 @@
+
+import 'dart:ui';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/painting.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'package:intl/intl.dart';
+import 'package:my_physio/CentreItem.dart';
+import 'package:my_physio/Providers/Services.dart';
+import 'package:my_physio/Providers/centres.dart';
+import 'package:my_physio/Providers/doctors.dart';
+import 'package:my_physio/ServiceItem.dart';
+
+import 'package:my_physio/carouselSlider.dart';
+
+// import 'package:my_physio/models/centres.dart';
+import 'package:my_physio/tabs_screen.dart';
+import 'package:provider/provider.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  TextEditingController _doctorName = TextEditingController();
+
+  var _isInit = true;
+  var _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+//  Provider.of<CentreProvider>(context).fetchAndSetProducts();
+    final fbm =FirebaseMessaging.instance;
+    fbm.requestPermission();
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+    });
+   _doctorName = new TextEditingController();
+  }
+
+  
+  @override
+  void didChangeDependencies() {
+    if (_isInit) {
+      setState(() {
+        _isLoading = true;
+      });
+        Provider.of<ServicesProvider>(context,listen: false).fetchAndSetServices().then((_) {
+       
+      Provider.of<CentreProvider>(context,listen: false).fetchAndSetProducts().then((_) {
+       Provider.of<DoctorProvider>(context,listen: false).fetchAndSetDoctors().then((_) {
+        setState(() {
+          _isLoading = false;
+        });
+       });
+      });
+       });
+    }
+    _isInit = false;
+    super.didChangeDependencies();
+  }
+
+
+  @override
+  void dispose() {
+    _doctorName.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+     final centresData = Provider.of<CentreProvider>(context,listen: false);
+     final servicesData = Provider.of<ServicesProvider>(context,listen: false);
+     final doctorsData = Provider.of<DoctorProvider>(context,listen: false);
+    String _message;
+    DateTime now = DateTime.now();
+    String _currentHour = DateFormat('kk').format(now);
+    int hour = int.parse(_currentHour);
+     _message='';
+    setState(
+      () {
+        if (hour >= 5 && hour < 12) {
+          _message = 'Good Morning';
+        } else if (hour >= 12 && hour <= 17) {
+          _message = 'Good Afternoon';
+        } else {
+          _message = 'Good Evening';
+        }
+      },
+    );
+    
+    return Scaffold(
+      backgroundColor: Colors.white,
+      key: _scaffoldKey,
+      
+      floatingActionButton:FloatingActionButton(
+        
+        child: Icon(Icons.app_registration_rounded),
+        onPressed: () => {
+                         Navigator.push(context,
+                        MaterialPageRoute(builder: (BuildContext context) {
+                        return TabsScreen(centresData,servicesData);
+                      }))
+
+
+        },
+        backgroundColor: Colors.blue[300],
+        tooltip: 'Appointments',
+        //label: Text("Book Appointment"),
+      ),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        actions: <Widget>[Container()],
+        backgroundColor: Colors.blue,
+        elevation: 0,
+        title: Container(
+          padding: EdgeInsets.only(top: 5),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                //width: MediaQuery.of(context).size.width/1.3,
+                alignment: Alignment.center,
+                child: Text(
+                  _message,
+                  style: GoogleFonts.lato(
+                    color: Colors.black54,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 55,
+              ),
+              IconButton(
+                splashRadius: 20,
+                icon: Icon(Icons.notifications_active),
+                onPressed: () {
+                  // Navigator.push(
+                  //     context,
+                  //     MaterialPageRoute(
+                  //         builder: (contex) => NotificationList()));
+                },
+              ),
+            ],
+          ),
+        ),
+        iconTheme: IconThemeData(
+          color: Colors.black,
+        ),
+      ),
+      body: SafeArea(
+        child: NotificationListener<OverscrollIndicatorNotification>(
+          onNotification: (OverscrollIndicatorNotification overscroll) {
+            overscroll.disallowGlow();
+             return true;
+          },
+          child: ListView(
+            physics: ClampingScrollPhysics(),
+            shrinkWrap: true,
+            children: <Widget>[
+              Column(
+                children: [
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    padding: EdgeInsets.only(left: 20, bottom: 10),
+                    child: Text(
+                      "Hello " + 'Divyansh',
+                      style: GoogleFonts.lato(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    padding: EdgeInsets.only(left: 20, bottom: 25),
+                    child: Text(
+                      "Let's Find Your\nDoctor",
+                      style: GoogleFonts.lato(
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(20, 0, 20, 25),
+                    child: TextFormField(
+                      textInputAction: TextInputAction.search,
+                      controller: _doctorName,
+                      decoration: InputDecoration(
+                        contentPadding:
+                            EdgeInsets.only(left: 20, top: 10, bottom: 10),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                          borderSide: BorderSide.none,
+                        ),
+                        filled: true,
+                        fillColor: Colors.grey[200],
+                        hintText: 'Search doctor',
+                        hintStyle: GoogleFonts.lato(
+                          color: Colors.black26,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w800,
+                        ),
+                        suffixIcon: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.blue[900]!.withOpacity(0.9),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: IconButton(
+                            iconSize: 20,
+                            splashRadius: 20,
+                            color: Colors.white,
+                            icon: Icon(FlutterIcons.search1_ant),
+                            onPressed: () {},
+                          ),
+                        ),
+                      ),
+                      style: GoogleFonts.lato(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w800,
+                      ),
+                      onFieldSubmitted: (String value) {
+                        setState(
+                          () {
+                            // value.length == 0
+                            //     ? Container()
+                            //     : Navigator.push(
+                            //         context,
+                            //         MaterialPageRoute(
+                            //           builder: (context) => SearchList(
+                            //             searchKey: value,
+                            //           ),
+                            //         ),
+                            //       );
+                          },
+                        );
+                      },
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(left: 23, bottom: 10),
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Our Doctors",
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.lato(
+                          color: Colors.blue[800],
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18),
+                    ),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: Carouselslider(doctorsData),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(left: 20),
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Our Specialities",
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.lato(
+                          color: Colors.blue[800],
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18),
+                    ),
+                  ),
+                  ServiceItem(servicesData.services),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(left: 20),
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Our Centres",
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.lato(
+                          color: Colors.blue[800],
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18),
+                    ),
+                  ),
+                  CentreItem(centresData)
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
